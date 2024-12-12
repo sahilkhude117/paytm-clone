6545273ab2b3500358e755136e808f7c599ea0ca
sahilkhude117 <sahilkhude11@gmail.com>
2024-12-12 21:30:35 +0530

    onramp

---
+++ apps/user-app/app/lib/actions/createOnrampTransaction.ts
@@ -0,0 +1,31 @@
+'use server';
+
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth";
+import prisma from "@repo/db/client";
+
+export async function createOnRampTransction(amount: number, provider: string){
+    const session = await getServerSession(authOptions);
+    const userId = session.user.id;
+    const token = Math.random().toString();
+
+    if(!userId){
+        return {
+            message : "User not logged in"
+        }
+    }
+    await prisma.onRampTransaction.create({
+        data : {
+            userId: Number(userId),
+            amount: amount,
+            status:"Processing",
+            startTime: new Date(),
+            provider,
+            token:token,
+        }
+    })
+
+    return {
+        message : "On ramp transction success"
+    }
+}
